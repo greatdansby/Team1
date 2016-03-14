05d7f1a9e1807920b17ae9c2ee0770012e062668
Daniel Mitus <dmitus3@gatech.edu>
2016-03-14 19:38:36 -0400

    Lots of stuff

--- TestHI/src/com/example/testhi/MainActivity.java
+++ TestHI/src/com/example/testhi/MainActivity.java
@@ -1,34 +1,44 @@
 package com.example.testhi;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+
+	    TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+	    tabHost.setup();
+
+	    TabSpec tab0 = tabHost.newTabSpec("Home");
+	    TabSpec tab1 = tabHost.newTabSpec("My Records & Stats");
+	    TabSpec tab2 = tabHost.newTabSpec("Surveys & Quizzes");
+	    TabSpec tab3 = tabHost.newTabSpec("Third Party Connections");
+
+	    tab0.setIndicator("Home");
+	    tab0.setContent(new Intent(this,HomeActivity.class));
+	    
+	    tab1.setIndicator("My Records & Stats");
+	    tab1.setContent(new Intent(this,ReviewEhrActivity.class));
+
+	    tab2.setIndicator("Surveys & Quizzes");
+	    tab2.setContent(new Intent(this,SurveyActivity.class));
+
+	    tab3.setIndicator("Third Party Connections");
+	    tab3.setContent(new Intent(this,ThirdpartyActivity.class));
+
+	    tabHost.addTab(tab0);
+	    tabHost.addTab(tab1);
+	    tabHost.addTab(tab2);
+	    tabHost.addTab(tab3);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
