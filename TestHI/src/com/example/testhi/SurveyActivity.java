e71bd67f3554d4915201d4acac32fb84a4e05dff
Daniel Mitus <dmitus3@gatech.edu>
2016-03-17 23:20:50 -0400

    Pulls survey list and survey questions

--- TestHI/src/com/example/testhi/SurveyActivity.java
+++ TestHI/src/com/example/testhi/SurveyActivity.java
@@ -1,5 +1,7 @@
 package com.example.testhi;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,8 +12,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SurveyActivity extends Activity implements myInterface {
@@ -30,13 +36,13 @@ public class SurveyActivity extends Activity implements myInterface {
 
 	@Override
 	public void processFinish(String output, String id) {
+		JSONObject e;
+		JSONArray jArray;
+		LinearLayout parent;
+		LinearLayout child;
 		switch(id) {
-			case "a":
-				
-				JSONObject e;
-				JSONArray jArray;
-				LinearLayout parent;
-				LinearLayout child;
+			case "a": //Populate survey list
+						
 				try {
 					
 					jArray = new JSONArray(output);
@@ -49,6 +55,13 @@ public class SurveyActivity extends Activity implements myInterface {
 			            child = (LinearLayout) inflater.inflate(R.layout.survey, null);
 			            TextView txt = (TextView) child.findViewById(R.id.survey_name);
 			            txt.setText(p.getString("survey_name"));
+			            TextView btn = (TextView) child.findViewById(R.id.survey_button);
+			            btn.setTag(p.getInt("id"));
+			            btn.setOnClickListener(new OnClickListener(){
+			            	public void onClick(View v){
+			            		fillSurvey(v);
+			            	}
+			            });
 			            child.setTag(p.getInt("id"));
 			            parent.addView(child);
 			        }
@@ -57,11 +70,46 @@ public class SurveyActivity extends Activity implements myInterface {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
+				return;
+			case "b": //Populate survey questions
+
+				try {
+					
+					jArray = new JSONArray(output);
+					parent = (LinearLayout) findViewById(R.id.surveyQuestions);
+					parent.removeAllViews();
+					LayoutInflater inflater = (LayoutInflater)getBaseContext() .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+					
+			        for(int i = 0; i < jArray.length(); i++) {
+			            JSONObject p = (JSONObject) jArray.get(i);
+			            child = (LinearLayout) inflater.inflate(R.layout.survey_question, null);
+			            TextView txt = (TextView) child.findViewById(R.id.questionText);
+			            txt.setText(p.getString("surveyQuestion_text"));
+			            Spinner dropdown = (Spinner) child.findViewById(R.id.answerDropdown);
+			            ArrayList<String> spinnerArray = new ArrayList<String>();
+			            spinnerArray.add("Yes");
+			            spinnerArray.add("No");
+			            ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray); //selected item will look like a spinner set from XML
+			            spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			            dropdown.setAdapter(spinnerArrayAdapter);
+			            child.setTag(p.getInt("id"));
+			            parent.addView(child);
+			        }
+				
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				return;
 			}		
 	}
-	public void setSurvey(){
-		Log.d("info","Setting Survey");
+
+	public void fillSurvey(View v) {
+		
+		fetchDataFromAppServer fetchSurveyQuestions = new fetchDataFromAppServer();
+		fetchSurveyQuestions.delegate = this;
+		fetchSurveyQuestions.execute("b","GetSurveyQuestions/?surveyId="+v.getTag());
 		
 	}
-		
+	
 }
