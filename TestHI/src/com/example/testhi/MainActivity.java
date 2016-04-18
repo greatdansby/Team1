c43243bc14f2a34273bb77429a9290dede1a8bb4
dgonzalez42 <dgonzalez42@gatech.edu>
2016-04-18 13:26:07 -0500

    Experimenting with formatting text.

--- TestHI/src/com/example/testhi/MainActivity.java
+++ TestHI/src/com/example/testhi/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
